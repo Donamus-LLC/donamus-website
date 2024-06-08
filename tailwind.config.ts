3fdf9d619069b0c5cd03287db7c135a14a5001e5
Deepankar Malhan <dmalhan3@gatech.edu>
2024-06-08 10:39:16 -0400

    feat: Added the Donamus colors to the Tailwind theme.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
@@ -8,11 +9,53 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        'sans': ['Cabin', 'Montserrat', ...fontFamily.sans]
       },
+      colors: {
+        donamus: {
+          primary: {
+            '50': '#FEF1ED',
+            '100': '#FCD6C9',
+            '200': '#F4AC93',
+            '300': '#E48F71',
+            '400': '#CD7659',
+            '500': '#BC6446',
+            '600': '#A14B2D',
+            '700': '#792F15',
+            '800': '#5E2410',
+            '900': '#321207'
+          },
+          secondary: {
+            '50': '#CFE4F6',
+            '100': '#92B9DC',
+            '200': '#6893BA',
+            '300': '#3D6E9B',
+            '400': '#1C4C78',
+            '500': '#092C4C',
+            '600': '#091F33',
+            '700': '#071521',
+            '800': '#010D17',
+            '900': '#03090F'
+          },
+          muted: {
+            '50': '#FAFAFA',
+            '100': '#F0F0F0',
+            '200': '#DCDDDD',
+            '300': '#C2C3C3',
+            '400': '#8C8D8D',
+            '500': '#6D6F6F',
+            '600': '#505353',
+            '700': '#313131',
+            '800': '#212222',
+            '900': '#141313'
+          },
+          accent: {
+            'yellow': '#F2CD79',
+            'green': '#BED7D1'
+          }
+        }
+      }
     },
   },
   plugins: [],
